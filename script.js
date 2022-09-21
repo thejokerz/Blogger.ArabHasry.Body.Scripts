1c3d67f4a65f89822839f1ae34632a72fb9b9c41
Mohsen Abdo <79073660+thejokerz@users.noreply.github.com>
2022-09-21 17:31:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -132,8 +132,6 @@ $(_0x33fe('0x34'))['length'] ? ($(_0x33fe('0x18'))[_0x33fe('0x13')](), $(_0x33fe
 
 
 // Cookie Message
-&lt;script data-cfasync='false' src='https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js'/>
-
 window.cookieconsent.initialise({
   &quot;palette&quot;: {
     &quot;popup&quot;: {

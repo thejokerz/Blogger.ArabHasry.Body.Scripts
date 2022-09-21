0b9080e393ee2ae21f3d07cf39aabc5580338c69
Mohsen Abdo <79073660+thejokerz@users.noreply.github.com>
2022-09-21 07:39:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-<!-- Start Body Scripts -->
-  
-<!-- View More Posts -->
-<!-- Button Open Side Menu -->
-<!-- Area Press To Hide For Side Menu And Search -->
 var ArrowIcon = &quot;<svg aria-hidden='true' focusable='false' role='img' viewBox='0 0 256 512' xmlns='http://www.w3.org/2000/svg'><path d='M119.5 326.9L3.5 209.1c-4.7-4.7-4.7-12.3 0-17l7.1-7.1c4.7-4.7 12.3-4.7 17 0L128 287.3l100.4-102.2c4.7-4.7 12.3-4.7 17 0l7.1 7.1c4.7 4.7 4.7 12.3 0 17L136.5 327c-4.7 4.6-12.3 4.6-17-.1z' fill='currentColor'/></svg>&quot;,BlogID = String(7701759513117874599);
 
 var _0x1ec6 = ['YS5vcC1vbmU=', 'Lm1vcC1saW5rcw==', 'Ym9keSAq', 'aGlkZQ==', 'LnllbWVuIHNwYW4=', 'bG9hZA==', 'cGFyZW50RWxlbWVudA==', 'aW5pdGlhbA==', 'c2V0QXR0cmlidXRl', 'c2Nyb2xsVG9w', 'eXRpbWcuY29t', 'cmVhZHk=', 'Y3Nz', 'Lm5vTW9yZVBvc3Rz', 'aW1nWw==', 'ZGF0YS1zcmM=', 'cGx1c2VsaW5r', 'ZmFkZU91dA==', 'ZmxleA==', 'dmlzaWJsZQ==', 'dGV4dA==', 'dG9nZ2xl', 'aHRtbA==', 'I1N0b3BTaXRreUhlYWRhcg==', 'Y2VpbA==', 'Zm9udC13ZWlnaHQ6IDcwMDtjb2xvcjogIzM1NjBhYjtmb250LXNpemU6MjFweA==', 'aW5uZXJIZWlnaHQ=', 'PGEgaHJlZj0iaHR0cHM6Ly93d3cuc2VvcGx1cy10ZW1wbGF0ZS5jb20iIHJlbD0ibm9mb2xsb3cgbm9vcGVuZXIiIHRhcmdldD0iX2JsYW5rIiB0aXRsZT0iU2VvUGx1cyBUZW1wbGF0ZSIgc3R5bGU9ImRpc3BsYXk6IGJsb2NrOyB2aXNpYmlsaXR5OiB2aXNpYmxlOyB3aWR0aDogMjdweDsgaGVpZ2h0OiAyN3B4OyBvcGFjaXR5OiAxOyB0cmFuc2Zvcm06IGluaXRpYWw7Ij48aW1nIGFsdD0iU2VvUGx1cyBUZW1wbGF0ZSIgc3JjPSIvLzMuYnAuYmxvZ3Nwb3QuY29tLy1ULVYtUEpPVTR2MC9YWll6SGZxMWRZSS9BQUFBQUFBQUJPdy9vYno4ck1jd0tnRXZQa0hQMWFoTTJ0eUFxbThmUllaWXdDSzRCR0FZWUN3L3cyNy1oMjctcC1rLW51LzI1Ni0yNTYucG5nIiBzdHlsZT0iZGlzcGxheTogYmxvY2s7IHZpc2liaWxpdHk6IHZpc2libGU7IHdpZHRoOiAyN3B4OyBoZWlnaHQ6IDI3cHg7IG9wYWNpdHk6IDE7IHRyYW5zZm9ybTogaW5pdGlhbDsiPjwvYT4=', 'Lm5hdi1wYXI=', 'I3RvcC1zb2NpYWwtTA==', 'LlBvc3RzLWJ5Q2F0ZWdvcnksLnBhZ2UtbmF2aWdhdGlvbiwuUGFnZVByYWtlZGl2', 'cmVwbGFjZQ==', 'LmJsb2ctcGFnZXItb2xkZXItbGluaw==', 'LnN0YXR1cy1tc2ctd3JhcA==', 'LnllbWVuIGltZ1thbHQ9IlNlb1BsdXMgVGVtcGxhdGUiXSwgLnllbWVuIGFbdGl0bGU9IlNlb1BsdXMgVGVtcGxhdGUiXQ==', 'YW5pbWF0ZQ==', 'YWN0aXZl', 'b2Zmc2V0V2lkdGg=', 'LnNlYXJjaC1zdWJtaXQyLCAuc3R4aw==', 'cHJldg==', 'Y2hpbGRyZW4=', 'cGFnZVlPZmZzZXQ=', 'aHJlZg==', 'LmxvYWRNb3JlUG9zdHMgYQ==', 'dG9w', 'L2RlZmF1bHQ=', 'YWRkQ2xhc3M=', 'Z2V0QXR0cmlidXRl', 'LmJvdHRvbS1mb290ZXIgLnllbWVuOmVxKDAp', 'LmxvYWRNb3JlID4gI2xvYWRlcg==', 'ZmFkZUlu', 'LnBhci10cCwgLmhlYWQtcHosIC5wYXItYm90dG0=', 'L21xZGVmYXVsdA==', 'LnNpZGVuYXY=', 'PHN0eWxlPmJvZHkuUkUge2JhY2tncm91bmQ6IHVybChodHRwczovLzQuYnAuYmxvZ3Nwb3QuY29tLy1OX0hGa29DTTBQWS9YYlg5TnNudUE4SS9BQUFBQUFBQUJXTS9qQXNsU2hkLWFQSUNBRHZPQW4zRHZPSVhKQThWaTdzQndDSzRCR0FZWUN3L3MxNjAwL3BfMTM5NWt5cmJtMS5wbmcpIG5vLXJlcGVhdCBjZW50ZXIgI2VlZTtiYWNrZ3JvdW5kLXBvc2l0aW9uOiBjZW50ZXIgY2VudGVyO2hlaWdodDogOTh2aDt9PC9zdHlsZT4=', 'YWRkRXZlbnRMaXN0ZW5lcg==', 'c2hvdw==', 'cmVtb3ZlQ2xhc3M=', 'cXVlcnlTZWxlY3Rvcg==', 'PHVsLz4=', 'QmxvZ2dlciBUZW1wbGF0ZQotLS0tLS0tLS0tLS0tLS0tLS0KPj4gUHJvZ3JhbW1lZCBieSA6IFNlb3BsdXMgZGV2ZWxvcGVycwo+PiBJbmZvcm1hdGlvbiAgIDogaHR0cHM6Ly93d3cuc2VvcGx1cy10ZW1wbGF0ZS5jb20K', 'c2Nyb2xs', 'MjdweA==', 'LnN0eGs=', 'Z2V0Qm91bmRpbmdDbGllbnRSZWN0', 'ZWFjaA==', 'bm90LXBs', 'cHJldmVudERlZmF1bHQ=', 'Lm1vcC1wYWdlcw==', 'bGVuZ3Ro', 'YXBwZW5k', 'JWNTZW9wbHVzIHY1LjMK', 'b3Blbg==', 'I0xpbmtMaXN0MjIgbGk=', 'cmVtb3ZlQXR0cmlidXRl', 'I0xpbmtMaXN0MjI=', 'LnNlYXJjaC1ib3g=', 'Lm1vcC1pY29u', 'Ym9keQ==', 'Y2xhc3NMaXN0', 'YWpheA==', 'LmJsb2ctcG9zdHM=', 'LnBvcy10LXQsIGEuY2xvc2VidG4=', 'LnllbWVu', 'LnNlYXJjaCBh', 'UExIb2xkZXI=', 'YXR0cg==', 'LmxvYWRNb3JlUG9zdHM=', 'PC9zcGFuPg==', 'YmxvY2s=', 'ZmluZA==', 'cmVtb3Zl', 'Y2xpY2s=', 'aW5kZXhPZg=='];
@@ -56,9 +51,6 @@ $(_0x33fe('0x34'))['length'] ? ($(_0x33fe('0x18'))[_0x33fe('0x13')](), $(_0x33fe
     var _0x4c870e = _0x3a0190(_0x33fe('0x49'));
     _0x3a0190(window)[_0x33fe('0x53')](function() { 0x63 <= _0x3a0190(this)[_0x33fe('0x1f')]() ? _0x4c870e[_0x33fe('0x4e')](0xa)[_0x33fe('0x39')](_0x33fe('0x49'))[_0x33fe('0x44')](_0x33fe('0x3a')) : _0x4c870e[_0x33fe('0x39')](_0x33fe('0x49'))['removeClass'](_0x33fe('0x3a')); });
 }), $(_0x33fe('0x46'))[_0x33fe('0x5b')] && ($(_0x33fe('0xb'))[_0x33fe('0x22')]({ 'display': _0x33fe('0x28'), 'visibility': _0x33fe('0x29'), 'width': 'auto', 'height': 'auto', 'opacity': '1', 'transform': _0x33fe('0x1d') }), $(_0x33fe('0x38'))[_0x33fe('0x22')]({ 'display': _0x33fe('0x11'), 'visibility': 'visible', 'width': _0x33fe('0x54'), 'height': _0x33fe('0x54'), 'opacity': '1', 'transform': _0x33fe('0x1d') })), $($(_0x33fe('0x31')))['insertBefore'](_0x33fe('0x1a')));
-<!-- View More Posts -->
-<!-- Button Open Side Menu -->
-<!-- Area Press To Hide For Side Menu And Search -->
 
 
 
@@ -76,7 +68,8 @@ $(_0x33fe('0x34'))['length'] ? ($(_0x33fe('0x18'))[_0x33fe('0x13')](), $(_0x33fe
 
 
 
-<!-- Scroll To Top -->
+
+
   var scrollToTop = $(&quot;.scroll-to-top&quot;);
   $(window).on(&quot;scroll&quot;, function () {
     if ($(window).scrollTop() &gt;= 500) {
@@ -102,7 +95,7 @@ $(_0x33fe('0x34'))['length'] ? ($(_0x33fe('0x18'))[_0x33fe('0x13')](), $(_0x33fe
     &quot;use strict&quot;;
     $(&quot;.tutorial-demo style&quot;).detach().appendTo(&quot;head&quot;);
   });
-<!-- Scroll To Top -->
+
 
 
 
@@ -124,7 +117,6 @@ window.cookieconsent.initialise({
     &quot;href&quot;: &quot;https://arab-hasry.blogspot.com/p/privacy-policy.html&quot;
   }
 });
-<!-- Cookie Message -->
 
 
 
@@ -143,7 +135,8 @@ window.cookieconsent.initialise({
 
 
 
-<!-- Dark Mode -->
+
+
 // check for saved &#39;darkMode&#39; in localStorage
 let darkMode = localStorage.getItem(&#39;darkMode&#39;); 
 
@@ -182,7 +175,6 @@ darkModeToggle.addEventListener(&#39;click&#39;, () =&gt; {
     disableDarkMode(); 
   }
 });
-<!-- Dark Mode -->
 
 
 
@@ -200,7 +192,8 @@ darkModeToggle.addEventListener(&#39;click&#39;, () =&gt; {
 
 
 
-<!-- Sticky The Sidebar When The User Scrolling Up Or Down -->
+
+
  !function(i) {
     i.fn.theiaStickySidebar=function(t) {
         var e,
@@ -364,14 +357,13 @@ darkModeToggle.addEventListener(&#39;click&#39;, () =&gt; {
 var $StickySidebar = jQuery.noConflict();
 $StickySidebar(document).ready(function(){
     "use strict";
-/*--------------------- //StickySidebar ---------------------*/
+
     $StickySidebar(".sidebar").theiaStickySidebar({
         additionalMarginTop: 90,
          additionalMarginBottom: -15,
     });
 
 }); // End $StickySidebar(document).ready(function(){
-<!-- Sticky The Sidebar When The User Scrolling Up Or Down -->
 
 
 
@@ -390,7 +382,8 @@ $StickySidebar(document).ready(function(){
 
 
 
-<!-- News Ticker and Slider Show -->
+
+
 var b_list=document.getElementById("b-list")!==null?document.getElementById("b-list").innerHTML:6;var b_cover=document.getElementById("b-cover")!==null?document.getElementById("b-cover").innerHTML:7;var b_sided=document.getElementById("b-sided")!==null?document.getElementById("b-sided").innerHTML:6;var b_cards=document.getElementById("b-cards")!==null?document.getElementById("b-cards").innerHTML:6;var m_res=document.getElementById("max-results")!==null?document.getElementById("max-results").innerHTML:10;var imags_n=document.getElementById("g-max")!==null?document.getElementById("g-max").innerHTML:9;var int_autoplay=document.getElementById("int-autoplay")!==null?(document.getElementById("int-autoplay").innerHTML=='false'?false:true):true;var rel_autoplay=document.getElementById("rel-autoplay")!==null?(document.getElementById("rel-autoplay").innerHTML=='true'?true:false):false;var alt_Img=document.getElementById("alt-Image")!==null?document.getElementById("alt-Image").innerHTML:'http://4.bp.blogspot.com/-MVB5OaBXstY/VpvWQcpZhZI/AAAAAAAAAqM/2MyUX_xVoc0/s1600-r/alt.png';var SelecT=document.getElementById("select");SelecT!==null&&SelecT.innerText=="false"?document.getElementsByTagName("body")[0].classList.remove("no-select"):"";var Skin=document.getElementById("fb-skin")!==null?document.getElementById("fb-skin").innerHTML:'light';var int_items=document.getElementById("int-items")!==null?document.getElementById("int-items").innerHTML:'3';var Profile=document.getElementById("au-profile")!==null?document.getElementById("au-profile").innerHTML:'true';
 
 
@@ -1102,7 +1095,6 @@ $(function () {
                         $(".f-cols")["addClass"]("no-vp")
                 }
         })
-<!-- News Ticker and Slider Show -->
 
 
 
@@ -1123,7 +1115,8 @@ $(function () {
 
 
 
-<!-- Hide And Show Top Main Menu When The User Scrolling Up Or Down -->
+
+
 (function(){
 
     var doc = document.documentElement;
@@ -1176,7 +1169,6 @@ $(function () {
     window.addEventListener('scroll', checkScroll);
 
 })();
-<!-- Hide And Show Top Main Menu When The User Scrolling Up Or Down -->
 
 
 
@@ -1193,7 +1185,8 @@ $(function () {
 
 
 
-<!-- Add And Remove Active Class Based On URL -->
+
+
     jQuery(function($) {
      var path = window.location.href;
      $('ul li a').each(function() {
@@ -1202,7 +1195,6 @@ $(function () {
       }
      });
     });
-<!-- Add And Remove Active Class Based On URL -->
 
 
 
@@ -1224,7 +1216,8 @@ $(function () {
 
 
 
-<!-- Dropdown Inside Top Main Menu -->
+
+
 // Get all the dropdown from document
 document.querySelectorAll(&#39;.dropdown-toggle&#39;).forEach(dropDownFunc);
 document.querySelectorAll(&#39;.submenu&#39;).forEach(submenuFunc);
@@ -1303,7 +1296,7 @@ document.querySelectorAll(&#39;.dropdown-menu&#39;).forEach(function (dropDownLi
 });
 
 
-/*---- Submenu Logic ----*/
+
 
 // submenu
 function submenuFunc(submenuHover) {
@@ -1336,4 +1329,3 @@ function submenuClose() {
         subMenu.classList.remove(&#39;submenu-open&#39;);
     });
 }
-<!-- Dropdown Inside Top Main Menu -->

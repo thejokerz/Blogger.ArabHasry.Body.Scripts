7209bf06d30f393aed37510acd2999f4575276c5
Mohsen Abdo <79073660+thejokerz@users.noreply.github.com>
2022-09-24 14:20:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -350,6 +350,14 @@ $StickySidebar(document).ready(function(){
 
 
 // Start News Ticker and Slider Show
+var b_list=document.getElementById("b-list")!==null?document.getElementById("b-list").innerHTML:6;var b_cover=document.getElementById("b-cover")!==null?document.getElementById("b-cover").innerHTML:7;var b_sided=document.getElementById("b-sided")!==null?document.getElementById("b-sided").innerHTML:6;var b_cards=document.getElementById("b-cards")!==null?document.getElementById("b-cards").innerHTML:6;var m_res=document.getElementById("max-results")!==null?document.getElementById("max-results").innerHTML:10;var imags_n=document.getElementById("g-max")!==null?document.getElementById("g-max").innerHTML:9;var int_autoplay=document.getElementById("int-autoplay")!==null?(document.getElementById("int-autoplay").innerHTML=='false'?false:true):true;var rel_autoplay=document.getElementById("rel-autoplay")!==null?(document.getElementById("rel-autoplay").innerHTML=='true'?true:false):false;var alt_Img=document.getElementById("alt-Image")!==null?document.getElementById("alt-Image").innerHTML:'http://4.bp.blogspot.com/-MVB5OaBXstY/VpvWQcpZhZI/AAAAAAAAAqM/2MyUX_xVoc0/s1600-r/alt.png';var SelecT=document.getElementById("select");SelecT!==null&&SelecT.innerText=="false"?document.getElementsByTagName("body")[0].classList.remove("no-select"):"";var Skin=document.getElementById("fb-skin")!==null?document.getElementById("fb-skin").innerHTML:'light';var int_items=document.getElementById("int-items")!==null?document.getElementById("int-items").innerHTML:'3';var Profile=document.getElementById("au-profile")!==null?document.getElementById("au-profile").innerHTML:'true';
+
+var functions = {
+}
+$.fn.timeago = function (action) {
+        var fn = action ? functions[action] : functions;
+};
+
 jQuery("abbr.timeago")["timeago"]();
 var globals = {
         sidebar: $("aside")["offset"]()

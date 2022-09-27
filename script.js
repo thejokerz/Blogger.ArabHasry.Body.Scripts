f072bec5e87a0e4695e401d2a6381d4ee2df747d
Mohsen Abdo <79073660+thejokerz@users.noreply.github.com>
2022-09-28 00:26:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 // Start Body Scripts
-
 // View More Posts
 // Button Open Side Menu
 // Area Press To Hide For Side Menu And Search
 var ArrowIcon = "<svg aria-hidden='true' focusable='false' role='img' viewBox='0 0 256 512' xmlns='http://www.w3.org/2000/svg'><path d='M119.5 326.9L3.5 209.1c-4.7-4.7-4.7-12.3 0-17l7.1-7.1c4.7-4.7 12.3-4.7 17 0L128 287.3l100.4-102.2c4.7-4.7 12.3-4.7 17 0l7.1 7.1c4.7 4.7 4.7 12.3 0 17L136.5 327c-4.7 4.6-12.3 4.6-17-.1z' fill='currentColor'/></svg>",BlogID = String(7701759513117874599);
-
 var _0x1ec6 = ['YS5vcC1vbmU=', 'Lm1vcC1saW5rcw==', 'Ym9keSAq', 'aGlkZQ==', 'LnllbWVuIHNwYW4=', 'bG9hZA==', 'cGFyZW50RWxlbWVudA==', 'aW5pdGlhbA==', 'c2V0QXR0cmlidXRl', 'c2Nyb2xsVG9w', 'eXRpbWcuY29t', 'cmVhZHk=', 'Y3Nz', 'Lm5vTW9yZVBvc3Rz', 'aW1nWw==', 'ZGF0YS1zcmM=', 'cGx1c2VsaW5r', 'ZmFkZU91dA==', 'ZmxleA==', 'dmlzaWJsZQ==', 'dGV4dA==', 'dG9nZ2xl', 'aHRtbA==', 'I1N0b3BTaXRreUhlYWRhcg==', 'Y2VpbA==', 'Zm9udC13ZWlnaHQ6IDcwMDtjb2xvcjogIzM1NjBhYjtmb250LXNpemU6MjFweA==', 'aW5uZXJIZWlnaHQ=', 'PGEgaHJlZj0iaHR0cHM6Ly93d3cuc2VvcGx1cy10ZW1wbGF0ZS5jb20iIHJlbD0ibm9mb2xsb3cgbm9vcGVuZXIiIHRhcmdldD0iX2JsYW5rIiB0aXRsZT0iU2VvUGx1cyBUZW1wbGF0ZSIgc3R5bGU9ImRpc3BsYXk6IGJsb2NrOyB2aXNpYmlsaXR5OiB2aXNpYmxlOyB3aWR0aDogMjdweDsgaGVpZ2h0OiAyN3B4OyBvcGFjaXR5OiAxOyB0cmFuc2Zvcm06IGluaXRpYWw7Ij48aW1nIGFsdD0iU2VvUGx1cyBUZW1wbGF0ZSIgc3JjPSIvLzMuYnAuYmxvZ3Nwb3QuY29tLy1ULVYtUEpPVTR2MC9YWll6SGZxMWRZSS9BQUFBQUFBQUJPdy9vYno4ck1jd0tnRXZQa0hQMWFoTTJ0eUFxbThmUllaWXdDSzRCR0FZWUN3L3cyNy1oMjctcC1rLW51LzI1Ni0yNTYucG5nIiBzdHlsZT0iZGlzcGxheTogYmxvY2s7IHZpc2liaWxpdHk6IHZpc2libGU7IHdpZHRoOiAyN3B4OyBoZWlnaHQ6IDI3cHg7IG9wYWNpdHk6IDE7IHRyYW5zZm9ybTogaW5pdGlhbDsiPjwvYT4=', 'Lm5hdi1wYXI=', 'I3RvcC1zb2NpYWwtTA==', 'LlBvc3RzLWJ5Q2F0ZWdvcnksLnBhZ2UtbmF2aWdhdGlvbiwuUGFnZVByYWtlZGl2', 'cmVwbGFjZQ==', 'LmJsb2ctcGFnZXItb2xkZXItbGluaw==', 'LnN0YXR1cy1tc2ctd3JhcA==', 'LnllbWVuIGltZ1thbHQ9IlNlb1BsdXMgVGVtcGxhdGUiXSwgLnllbWVuIGFbdGl0bGU9IlNlb1BsdXMgVGVtcGxhdGUiXQ==', 'YW5pbWF0ZQ==', 'YWN0aXZl', 'b2Zmc2V0V2lkdGg=', 'LnNlYXJjaC1zdWJtaXQyLCAuc3R4aw==', 'cHJldg==', 'Y2hpbGRyZW4=', 'cGFnZVlPZmZzZXQ=', 'aHJlZg==', 'LmxvYWRNb3JlUG9zdHMgYQ==', 'dG9w', 'L2RlZmF1bHQ=', 'YWRkQ2xhc3M=', 'Z2V0QXR0cmlidXRl', 'LmJvdHRvbS1mb290ZXIgLnllbWVuOmVxKDAp', 'LmxvYWRNb3JlID4gI2xvYWRlcg==', 'ZmFkZUlu', 'LnBhci10cCwgLmhlYWQtcHosIC5wYXItYm90dG0=', 'L21xZGVmYXVsdA==', 'LnNpZGVuYXY=', 'PHN0eWxlPmJvZHkuUkUge2JhY2tncm91bmQ6IHVybChodHRwczovLzQuYnAuYmxvZ3Nwb3QuY29tLy1OX0hGa29DTTBQWS9YYlg5TnNudUE4SS9BQUFBQUFBQUJXTS9qQXNsU2hkLWFQSUNBRHZPQW4zRHZPSVhKQThWaTdzQndDSzRCR0FZWUN3L3MxNjAwL3BfMTM5NWt5cmJtMS5wbmcpIG5vLXJlcGVhdCBjZW50ZXIgI2VlZTtiYWNrZ3JvdW5kLXBvc2l0aW9uOiBjZW50ZXIgY2VudGVyO2hlaWdodDogOTh2aDt9PC9zdHlsZT4=', 'YWRkRXZlbnRMaXN0ZW5lcg==', 'c2hvdw==', 'cmVtb3ZlQ2xhc3M=', 'cXVlcnlTZWxlY3Rvcg==', 'PHVsLz4=', 'QmxvZ2dlciBUZW1wbGF0ZQotLS0tLS0tLS0tLS0tLS0tLS0KPj4gUHJvZ3JhbW1lZCBieSA6IFNlb3BsdXMgZGV2ZWxvcGVycwo+PiBJbmZvcm1hdGlvbiAgIDogaHR0cHM6Ly93d3cuc2VvcGx1cy10ZW1wbGF0ZS5jb20K', 'c2Nyb2xs', 'MjdweA==', 'LnN0eGs=', 'Z2V0Qm91bmRpbmdDbGllbnRSZWN0', 'ZWFjaA==', 'bm90LXBs', 'cHJldmVudERlZmF1bHQ=', 'Lm1vcC1wYWdlcw==', 'bGVuZ3Ro', 'YXBwZW5k', 'JWNTZW9wbHVzIHY1LjMK', 'b3Blbg==', 'I0xpbmtMaXN0MjIgbGk=', 'cmVtb3ZlQXR0cmlidXRl', 'I0xpbmtMaXN0MjI=', 'LnNlYXJjaC1ib3g=', 'Lm1vcC1pY29u', 'Ym9keQ==', 'Y2xhc3NMaXN0', 'YWpheA==', 'LmJsb2ctcG9zdHM=', 'LnBvcy10LXQsIGEuY2xvc2VidG4=', 'LnllbWVu', 'LnNlYXJjaCBh', 'UExIb2xkZXI=', 'YXR0cg==', 'LmxvYWRNb3JlUG9zdHM=', 'PC9zcGFuPg==', 'YmxvY2s=', 'ZmluZA==', 'cmVtb3Zl', 'Y2xpY2s=', 'aW5kZXhPZg=='];
 (function(_0x52cd66, _0x1ec60b) {
     var _0x33fee0 = function(_0x34e3fb) { while (--_0x34e3fb) { _0x52cd66['push'](_0x52cd66['shift']()); } };
@@ -62,9 +60,6 @@ $(_0x33fe('0x34'))['length'] ? ($(_0x33fe('0x18'))[_0x33fe('0x13')](), $(_0x33fe
 
 
 
-
-
-
 // Scroll To Top
   var scrollToTop = $(".scroll-to-top");
   $(window).on("scroll", function () {
@@ -86,7 +81,6 @@ $(_0x33fe('0x34'))['length'] ? ($(_0x33fe('0x18'))[_0x33fe('0x13')](), $(_0x33fe
       1000
     );
   });
-
   $(window).on("load", function () {
     "use strict";
     $(".tutorial-demo style").detach().appendTo("head");
@@ -95,40 +89,31 @@ $(_0x33fe('0x34'))['length'] ? ($(_0x33fe('0x18'))[_0x33fe('0x13')](), $(_0x33fe
 
 
 
-
-
-
 // Dark Mode
 // check for saved 'darkMode' in localStorage
 let darkMode = localStorage.getItem('darkMode'); 
-
 const darkModeToggle = document.querySelector('#chk');
-
 const enableDarkMode = () => {
   // 1. Add the class to the body
   document.body.classList.add('darkmode');
   // 2. Update darkMode in localStorage
   localStorage.setItem('darkMode', 'enabled');
 }
-
 const disableDarkMode = () => {
   // 1. Remove the class from the body
   document.body.classList.remove('darkmode');
   // 2. Update darkMode in localStorage 
   localStorage.setItem('darkMode', null);
 }
- 
 // If the user already visited and enabled darkMode
 // start things off with it on
 if (darkMode === 'enabled') {
   enableDarkMode();
 }
-
 // When someone clicks the button
 darkModeToggle.addEventListener('click', () => {
   // get their darkMode setting
   darkMode = localStorage.getItem('darkMode'); 
-  
   // if it not current enabled, enable it
   if (darkMode !== 'enabled') {
     enableDarkMode();
@@ -138,9 +123,6 @@ darkModeToggle.addEventListener('click', () => {
   }
 });
 // Dark Mode
-
-
-
 
 
 
@@ -304,40 +286,30 @@ darkModeToggle.addEventListener('click', () => {
     }
 }
 (jQuery);
-
 // Sticky Sidebar
 var $StickySidebar = jQuery.noConflict();
 $StickySidebar(document).ready(function(){
     "use strict";
-
     $StickySidebar(".sidebar").theiaStickySidebar({
         additionalMarginTop: 90,
          additionalMarginBottom: -15,
     });
-
 }); // End $StickySidebar(document).ready(function(){
 // Sticky The Sidebar When The User Scrolling Up Or Down
 
 
 
-
-
-
 // Hide And Show Top Main Menu When The User Scrolling Up Or Down
 (function(){
-
     var doc = document.documentElement;
     var w   = window;
-
     var curScroll;
     var prevScroll = w.scrollY || doc.scrollTop;
     var curDirection = 0;
     var prevDirection = 0;
-
     var header = document.getElementById('site-header');
     var toggled;
     var threshold = 0;
-
     var checkScroll = function() {
         curScroll = w.scrollY || doc.scrollTop;
         if(curScroll > prevScroll) {
@@ -348,17 +320,14 @@ $StickySidebar(document).ready(function(){
             //scrolled up
             curDirection = 1;
         }
-
         if(curDirection !== prevDirection) {
             toggled = toggleHeader();
         }
-
         prevScroll = curScroll;
         if(toggled) {
             prevDirection = curDirection;
         }
     };
-
     var toggleHeader = function() { 
         toggled = true;
         if(curDirection === 2 && curScroll > threshold) {
@@ -372,14 +341,9 @@ $StickySidebar(document).ready(function(){
         }
         return toggled;
     };
-
     window.addEventListener('scroll', checkScroll);
-
 })();
 // Hide And Show Top Main Menu When The User Scrolling Up Or Down
-
-
-
 
 
 
@@ -396,25 +360,19 @@ $StickySidebar(document).ready(function(){
 
 
 
-
-
-
 // Dropdown Inside Top Main Menu
 // Get all the dropdown from document
 document.querySelectorAll('.dropdown-toggle').forEach(dropDownFunc);
 document.querySelectorAll('.submenu').forEach(submenuFunc);
-
 // Dropdown Open and Close function
 function dropDownFunc(dropDown) {
     if (dropDown.classList.contains('click-dropdown') === true) {
         dropDown.addEventListener('click', function (e) {
             e.preventDefault();
-
             if (this.nextElementSibling.classList.contains('dropdown-active') === true) {
                 // Close the clicked dropdown
                 this.parentElement.classList.remove('dropdown-open');
                 this.nextElementSibling.classList.remove('dropdown-active');
-
             } else {
                 // Close the opend dropdown
                 closeDropdown();
@@ -425,16 +383,12 @@ function dropDownFunc(dropDown) {
             }
         });
     }
-
     if (dropDown.classList.contains('hover-dropdown') === true) {
-
         dropDown.onmouseover = dropDown.onmouseout = dropdownHover;
-
         function dropdownHover(e) {
             if (e.type == 'mouseover') {
                 // Close the opend dropdown
                 closeDropdown();
-
                 // add the open and active class(Opening the DropDown)
                 this.parentElement.classList.add('dropdown-open');
                 this.nextElementSibling.classList.add('dropdown-active');
@@ -442,7 +396,6 @@ function dropDownFunc(dropDown) {
         }
     }
 };
-
 // Listen to the doc click
 window.addEventListener('click', function (e) {
     // Close the menu if click happen outside menu
@@ -451,19 +404,16 @@ window.addEventListener('click', function (e) {
         closeDropdown();
     }
 });
-
 // Close the openend Dropdowns
 function closeDropdown() {
     // remove the open and active class from other opened Dropdown (Closing the opend DropDown)
     document.querySelectorAll('.dropdown-container').forEach(function (container) {
         container.classList.remove('dropdown-open');
     });
-
     document.querySelectorAll('.dropdown-menu').forEach(function (menu) {
         menu.classList.remove('dropdown-active');
     });
 }
-
 // close the dropdown on mouse out from the dropdown list
 document.querySelectorAll('.dropdown-menu').forEach(function (dropDownList) {
     // close the dropdown after user leave the list
@@ -476,15 +426,12 @@ document.querySelectorAll('.dropdown-menu').forEach(function (dropDownList) {
         });
     };
 });
-
 /*---- Submenu Logic ----*/
-
 // submenu
 function submenuFunc(submenuHover) {
     // checking submenu mouse hover and mouse leave event
     submenuHover.onmouseover = submenuHover.onmouseout = submenu;
 };
-
 // submenu 
 function submenu(e) {
     // if mouse hover
@@ -502,7 +449,6 @@ function submenu(e) {
         submenuClose();
     }
 }
-
 // close other submenu
 function submenuClose() {
     // performing close function for all the open submenus
@@ -514,17 +460,12 @@ function submenuClose() {
 
 
 
-
-
-
 // Lazy Loading
 // Lazy Load AdSense
 var lazyadsense=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyadsense||0!=document.body.scrollTop&&!1===lazyadsense)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyadsense=!0)},!0);
 // Lazy Load AdSense Loading Delay
 function downloadJSAtOnload(){var e=document.createElement("script");e.src="https://cdn.jsdelivr.net/gh/imintweb/Js/adsensegurd.js",document.body.appendChild(e)}window.addEventListener?window.addEventListener("load",downloadJSAtOnload,!1):window.attachEvent?window.attachEvent("onload",downloadJSAtOnload):window.onload=downloadJSAtOnload;
 // Lazy Loading
-
-
 
 
 
@@ -551,12 +492,7 @@ window.cookieconsent.initialise({
 
 
 
-
-
-
 // Start NewsTicker
-var b_list=document.getElementById("b-list")!==null?document.getElementById("b-list").innerHTML:6;var b_cover=document.getElementById("b-cover")!==null?document.getElementById("b-cover").innerHTML:7;var b_sided=document.getElementById("b-sided")!==null?document.getElementById("b-sided").innerHTML:6;var b_cards=document.getElementById("b-cards")!==null?document.getElementById("b-cards").innerHTML:6;var m_res=document.getElementById("max-results")!==null?document.getElementById("max-results").innerHTML:10;var imags_n=document.getElementById("g-max")!==null?document.getElementById("g-max").innerHTML:9;var int_autoplay=document.getElementById("int-autoplay")!==null?(document.getElementById("int-autoplay").innerHTML=='false'?false:true):true;var rel_autoplay=document.getElementById("rel-autoplay")!==null?(document.getElementById("rel-autoplay").innerHTML=='true'?true:false):false;var alt_Img=document.getElementById("alt-Image")!==null?document.getElementById("alt-Image").innerHTML:'http://4.bp.blogspot.com/-MVB5OaBXstY/VpvWQcpZhZI/AAAAAAAAAqM/2MyUX_xVoc0/s1600-r/alt.png';var SelecT=document.getElementById("select");SelecT!==null&&SelecT.innerText=="false"?document.getElementsByTagName("body")[0].classList.remove("no-select"):"";var Skin=document.getElementById("fb-skin")!==null?document.getElementById("fb-skin").innerHTML:'light';var int_items=document.getElementById("int-items")!==null?document.getElementById("int-items").innerHTML:'3';var Profile=document.getElementById("au-profile")!==null?document.getElementById("au-profile").innerHTML:'true';
-
 var functions = {
 }
 $.fn.timeago = function (action) {
@@ -677,5 +613,4 @@ $(function() {
 
 })
 // End NewsTicker
-
 // End Body Scripts
